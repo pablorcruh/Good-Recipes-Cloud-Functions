ab8b96ce701c52c9176a286b0d20a643226cfc6d
pablorcruh <pablorcruh@gmail.com>
2019-09-03 12:18:21 -0500

    refactor funcion para enviar notificaciones

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,71 @@
 const functions = require('firebase-functions');
-const admin =  require ('firebase-admin');
+const admin = require('firebase-admin');
+const Firestore = require('@google-cloud/firestore');
+const PROJECTID = 'good-recipes';
+const firestore = new Firestore({
+    projectId: PROJECTID
+});
+
+let recipeAuthor = "";
+let recipeTitle = "";
+let recipeDescription = "";
+let registrationToken = [];
+let payload;
 admin.initializeApp(functions.config().firebase);
-exports.notifyNewRecipe =functions.firestore
+
+exports.getAuthor = functions.firestore
     .document('recipe/{recipe}')
-    .onCreate((docSnapshot) =>{
+    .onCreate((docSnapshot) => {
         recipeAuthor = docSnapshot.data()['author'];
         recipeTitle = docSnapshot.data()['title'];
         recipeDescription = docSnapshot.data()['description'];
-        //console.log('se ha creado una recete>>>>>>>>>'+recipe);
-        const payload = {notification:{
+        this.payload = {
+            notification: {
                 title: `${recipeTitle}`,
                 description: `${recipeDescription}`
             }
         };
-        return admin.messaging().sendToTopic("Recipe", payload)
-            .then((response)=>{
-                console.log('Notification sent successfull', response);
-                return true;
+        firestore.collection('users/')
+            .doc(recipeAuthor.toString())
+            .get()
+            .then(doc => {
+                let data = doc.data().followers;
+                let followerList = data.split(',');
+                return followerList.forEach((follower) => {
+                    this.firestore.collection('users/')
+                        .doc(follower.toString())
+                        .get()
+                        .then((userData) => {
+                            registrationToken.push(userData.data().token.toString());
+                            return admin.messaging().sendToDevice(registrationToken, this.payload)
+                                .then((response) => {
+                                    const stillRegisteredTokens = registrationToken
+                                    return response.results.forEach((result, index) => {
+                                        const error = result.error
+                                        if (error) {
+                                            const failedRegistrationToken = registrationToken[index]
+                                            console.error('blah', failedRegistrationToken, error)
+                                            if (error.code === 'messaging/invalid-registration-token'
+                                                || error.code === 'messaging/registration-token-not-registered') {
+                                                const failedIndex = stillRegisteredTokens.indexOf(failedRegistrationToken)
+                                                if (failedIndex > -1) {
+                                                    stillRegisteredTokens.splice(failedIndex, 1)
+                                                }
+                                            }
+                                        }
+                                    });
+
+                                })
+                                .catch((error) => {
+                                    return error;
+                                });
+                        })
+                        .catch((error) => {
+                            return error;
+                        });
+                });
             })
-            .catch((error)=>{
-                console.log('Notification send failed:', error);
+            .catch((error) => {
+                return error;
             });
     });
